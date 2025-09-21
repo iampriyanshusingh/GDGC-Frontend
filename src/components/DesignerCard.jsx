9cc7cc6085277234b1a315f66ee0eccd133c05e4
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-09-21 08:20:35 +0530

    `testing: Designer Card done`

--- src/components/DesignerCard.jsx
+++ src/components/DesignerCard.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const DesignerCard = () => {
   return (
     <div className="ml-56">
-      {/* Title */}
       <h1 className="text-2xl font-bold text-gray-900 leading-tight mt-16">
         AI SaaS Landing Page for Automation Platform - SaaS Website
       </h1>
